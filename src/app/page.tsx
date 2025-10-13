dbc84c288adc11f901ef4afc86dc87cab12e5022
h <h@gmail.com>
2025-10-13 22:39:49 +0200

    do goofy shader stuff (why is fwidth not working)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+import dynamic from "next/dynamic";
 import Link from "next/link";
 import { View } from "./view/View";
 
@@ -5,7 +7,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-stretch h-dvh">
       <div className="h-12 bg-blue-400 flex-none">
-        
+
       </div>
       <div className="flex-1 flex relative overflow-hidden bg-blue-200">
         <View />
--- src/app/view/View.tsx
+++ src/app/view/View.tsx
@@ -1,67 +1,14 @@
 "use client";
 
 import React, { useEffect, useRef, useState } from 'react'
-import mkRegl from "regl";
+import mkRegl, { Regl } from "regl";
+import { mat3, mat4, vec3 } from 'gl-matrix';
+import { ACTION_DEBUG_INFO } from 'next/dist/next-devtools/dev-overlay/shared';
+
 export function View() {
     const canvasRef = useRef<HTMLCanvasElement>(null!);
     useEffect(() => {
-        const regl = mkRegl({ canvas: canvasRef.current });
-        const drawTriangle = regl({
-
-            // Shaders in regl are just strings.  You can use glslify or whatever you want
-            // to define them.  No need to manually create shader objects.
-            frag: `
-    precision mediump float;
-    uniform vec4 color;
-    void main() {
-      gl_FragColor = color;
-    }`,
-
-            vert: `
-    precision mediump float;
-    attribute vec2 position;
-    void main() {
-      gl_Position = vec4(position, 0, 1);
-    }`,
-
-            // Here we define the vertex attributes for the above shader
-            attributes: {
-                // regl.buffer creates a new array buffer object
-                position: regl.buffer([
-                    [-2, -2],   // no need to flatten nested arrays, regl automatically
-                    [4, -2],    // unrolls them into a typedarray (default Float32)
-                    [4, 4]
-                ])
-                // regl automatically infers sane defaults for the vertex attribute pointers
-            },
-
-            uniforms: {
-                // This defines the color of the triangle to be a dynamic variable
-                color: regl.prop<any, any>('color')
-            },
-
-            // This tells regl the number of vertices to draw in this command
-            count: 3
-        })
-
-        // regl.frame() wraps requestAnimationFrame and also handles viewport changes
-        regl.frame(({ time }) => {
-            // clear contents of the drawing buffer
-            regl.clear({
-                color: [0, 0, 0, 0],
-                depth: 1
-            })
-
-            // draw a triangle using the command defined above
-            drawTriangle({
-                color: [
-                    Math.cos(time * .1),
-                    Math.sin(time * .8),
-                    Math.cos(time * .3),
-                    1
-                ]
-            })
-        })
+        draw(canvasRef.current);
     }, []);
     const [windowSize, setWindowSize] = useState([0, 0]);
     useEffect(() => {
@@ -71,6 +18,129 @@ export function View() {
     }, []);
     return <canvas className="w-full h-full" width={windowSize[0]} height={windowSize[1]} ref={canvasRef}></canvas>
 }
-export function Walls() {
 
+type vec3_ = [number, number, number];
+async function draw(canvas: HTMLCanvasElement) {
+    await import("dbgui");
+    const regl = mkRegl({ gl: canvas.getContext("webgl")!, attributes: { depth: true, antialias: true }, extensions: ["OES_standard_derivatives"] });
+    
+    let view = mat4.create();
+    view = mat4.lookAt(view, [0, 3, 10], [0, 0, -10], [0, 1, 0]);
+    const wallDrawFns = ([
+        [[10, .3, 10], [0, -5, -10], RGB(250, 227, 219)], // floor
+        [[10, .3, 10], [0, 5, -10], RGB(250, 227, 219)], // ceiling
+        [[.3, 10, 10], [-5, 0, -10], RGB(250, 227, 219)], // left wall
+        [[.3, 10, 10], [5, 0, -10], RGB(250, 227, 219)], // right wall
+        [[10, 10, .3], [0, 0, -15], RGB(250, 227, 219)], // back wall
+    ] as Array<[vec3_, vec3_, vec3_]>).map(([wallDims, wallPos, color]) => {
+        let drawCall = mkDrawBox(regl, ...wallDims);
+        return (a: any) => {
+            let model = mat4.create();
+            model = mat4.translate(model, model, wallPos);
+            let normalMat = mat3.normalFromMat4([], mat4.multiply([], model, view));
+            drawCall({ ...a, model: model, color, normalMat })
+        };
+    });
+    
+    const lightPos = [0, 5, -10];
+    dbgui()
+        .add("x", $slider(-20, 20, .2, () => lightPos[0]).onInput((n) => lightPos[0] = n))
+        .add("y", $slider(-10, 10, .2, () => lightPos[1]).onInput((n) => lightPos[1] = n))
+        .add("z", $slider(-10, 10, .2, () => lightPos[2]).onInput((n) => lightPos[2] = n))
+    const drawLightVis = mkDrawBox(regl, .6, .6, .6);
+    regl.frame(({ time }) => {
+        regl.clear({
+            color: [0, 255, 0, 255],
+            depth: 1
+        })
+        const lightVisModel = mat4.translate([], mat4.create(), lightPos);
+        drawLightVis({ view, lightPos, model: lightVisModel, color: [.2, .3, .4], normalMat: mat3.normalFromMat4([], mat4.mul([], view, lightVisModel)) })
+        wallDrawFns.forEach(drawWall => drawWall({ view, lightPos }));
+    })
 }
+export function mkDrawBox(regl: Regl, l: number, h: number, w: number) {
+    let hl = l / 2;
+    let hw = w / 2;
+    let hh = h / 2;
+    const cubePosition = [
+        [-hl, +hh, +hw], [+hl, +hh, +hw], [+hl, -hh, +hw], [-hl, -hh, +hw], // positive z face
+        [+hl, +hh, +hw], [+hl, +hh, -hw], [+hl, -hh, -hw], [+hl, -hh, +hw], // positive x face
+        [+hl, +hh, -hw], [-hl, +hh, -hw], [-hl, -hh, -hw], [+hl, -hh, -hw], // negative z face
+        [-hl, +hh, -hw], [-hl, +hh, +hw], [-hl, -hh, +hw], [-hl, -hh, -hw], // negative x face
+        [-hl, +hh, -hw], [+hl, +hh, -hw], [+hl, +hh, +hw], [-hl, +hh, +hw], // top face
+        [-hl, -hh, -hw], [+hl, -hh, -hw], [+hl, -hh, +hw], [-hl, -hh, +hw]  // bottom face
+    ]
+
+    const cubeUv = Array(6).fill(0).map(() => [[0.0, 0.0], [1.0, 0.0], [1.0, 1.0], [0.0, 1.0]]);
+    const cubeNormal = [[0, 0, 1], [1, 0, 0], [0, 0, -1], [-1, 0, 0], [0, 1, 0], [0, -1, 0]].map(a => [...a, ...a, ...a, ...a]);
+    const cubeElements = [
+        [2, 1, 0], [2, 0, 3],       // positive z face
+        [6, 5, 4], [6, 4, 7],       // positive x face
+        [10, 9, 8], [10, 8, 11],    // negative z face
+        [14, 13, 12], [14, 12, 15], // negative x face
+        [18, 17, 16], [18, 16, 19], // top face
+        [20, 21, 22], [23, 20, 22]  // bottom face
+    ]
+    return regl({
+        frag: `
+        #extension GL_OES_standard_derivatives : enable
+        precision highp float;
+    varying vec2 vUv;
+    varying vec3 vNormal;
+    varying vec3 vCenter;
+    uniform vec3 lightPos;
+    uniform vec3 color;
+    
+    void main() {
+        vec3 normal = normalize(vNormal.xyz);
+        float edge = length(fwidth(normal));
+        float edgeFactor = smoothstep(0.2, 0.2 + 0.05, edge);
+        vec3 c = vec3(mix(0., 1., edgeFactor));
+        //vec3(distance(vCenter, vRelPos) / 20.) * 
+        //c = vec3(0., 0., edge * 2000.);
+        gl_FragColor = vec4(c, 1.);
+        //gl_FragColor = vec4((normal + 1.) * .5, 1.);
+    }`,
+
+        vert: `
+    precision highp float;
+    attribute vec3 position;
+    attribute vec2 uv;
+    attribute vec3 normal;
+    varying vec2 vUv;
+    varying vec3 vNormal;
+    varying vec3 vCenter;
+    uniform mat4 projection, view, model;
+    uniform mat3 normalMat;
+    uniform vec3 objSize;
+    void main() {
+        vUv = uv;
+        vNormal = normalMat * normal;
+        gl_Position = projection * view * model * vec4(position, 1);
+        vCenter = vec3(model[0][3],model[1][3],model[2][3]);
+    }`,
+
+        attributes: {
+            position: cubePosition,
+            uv: cubeUv,
+            normal: cubeNormal
+        },
+        elements: cubeElements,
+        uniforms: {
+            model: regl.prop<any, any>("model"),
+            view: regl.prop<any, any>("view"),
+            normalMat: regl.prop<any, any>("normalMat"),
+            projection: ({ viewportWidth, viewportHeight }) =>
+                mat4.perspective([],
+                    Math.PI / 4,
+                    viewportWidth / viewportHeight,
+                    0.1,
+                    100),
+            objSize: [l, w, h],
+            lightPos: regl.prop<any, any>("lightPos"),
+            color: regl.prop<any, any>("color"),
+        },
+        cull: { enable: true }
+    });
+}
+function RGB(r: number, g: number, b: number) { return [r / 255, g / 255, b / 255] }
